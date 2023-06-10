99e78be70ed47d632beed490f0f5c361ef2550fa
samettigy <samettig@icloud.com>
2023-06-10 16:27:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+internal class Program
+{
+     static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetAllByCategories(2))
+        {
+            Console.WriteLine(product.ProductName);
+        }
+
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T,bool>> filter);
+
+        void Add(T entities);
+        void Update(T entities);
+        void Delete(T entities);
+
+        List<T> GetAllByCategory(int categoryId);
+    }
+}
